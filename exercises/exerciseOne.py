3c1a52f6dbaaecf0f5fb47032e3927fe84e5d206
Tuyet1 <43265115+Tuyet1@users.noreply.github.com>
2018-09-14 10:56:49 +0200

    Opdracht 2

--- exercises/exerciseOne.py
+++ exercises/exerciseOne.py
@@ -2,13 +2,16 @@
 import sys
 
 welcomeMessage = "Welkom bij de Valori FST intake opdracht. Werk zorgvuldig en netjes, succes!"
+motivatie =  " Mijn naam is tuyet en FST is waar ik thuis hoor. Graag wil ik u bedanken voor uw aandacht en overweging. Een uiteenzetting van mijn kwaliteiten en motivatie licht ik graag toe in een persoonlijk gesprek. Met vriendelijke groet, Tuyet Tran
+
 
 def printWelcomeText():
     welcome = welcomeMessage
     return welcome
 
 def printExercise():
-    return None
+    motivatie = motivatie
+    return motivatie
 
 if len(welcomeMessage) != 76:
     sys.exit("Nice try, but not quite right :)")
